87ae788a37f2f274f57f6bc837f3f8b6ccdf0d8e
kirisame19 <kirisamekari19@gmail.com>
2024-11-19 20:09:08 +0900

    modelのTestケース作成中

---
+++ src/core/test/test_models.py
@@ -0,0 +1,17 @@
+﻿"""
+モデルのテスト
+"""
+
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+from . import models
+
+def create_user(email="user@example.com", password="TestPass123"):
+    """新しいユーザーを作成する"""
+    return get_user_model().objects.create(email=email, password=password)
+
+
+class Model_Tests(TestCase):
+    """モデルをテストする"""
+    pass
