f7c335e10f63dd20f3d5b880c89d9177594f3d99
kirisame19 <kirisamekari19@gmail.com>
2024-11-25 11:13:27 +0900

    テスト作成中

--- src/core/test/test_models.py
+++ src/core/test/test_models.py
@@ -7,6 +7,7 @@ from django.contrib.auth import get_user_model
 
 from . import models
 
+
 def create_user(email="user@example.com", password="TestPass123"):
     """新しいユーザーを作成する"""
     return get_user_model().objects.create(email=email, password=password)
@@ -14,4 +15,12 @@ def create_user(email="user@example.com", password="TestPass123"):
 
 class Model_Tests(TestCase):
     """モデルをテストする"""
-    pass
+
+    def test_create_user_with_email_succesful(self):
+        """ """
+        email = "test@example.com"
+        password = "TestPass123"
+        user = get_user_model().objects.create(email=email, password=password)
+
+        self.assertEqual(user.email, email)
+        self.assertEqual(user.check_password(password))
