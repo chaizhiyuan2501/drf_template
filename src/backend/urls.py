efe4d8a4fb4cf0434e7654947ac6ec271008031b
kirisame19 <kirisamekari19@gmail.com>
2024-12-23 13:36:58 +0900

    アバター追加中

--- src/backend/urls.py
+++ src/backend/urls.py
@@ -2,20 +2,24 @@ from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularSwaggerView,
 )
-from rest_framework_simplejwt.views import TokenRefreshView,TokenVerifyView, TokenObtainPairView
+from rest_framework_simplejwt.views import (
+    TokenRefreshView,
+    TokenVerifyView,
+    TokenObtainPairView,
+)
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("schema/", SpectacularAPIView.as_view(), name="api-schema"),
     path(
-        "docs/",
-        SpectacularSwaggerView.as_view(url_name="api-schema"),
-        name="api-docs",
+        "docs/", SpectacularSwaggerView.as_view(url_name="api-schema"), name="api-docs"
     ),
-        path('login/', TokenObtainPairView.as_view(), name='login'),
-    path('token/refresh', TokenRefreshView.as_view(), name='token_refresh'), # token更新
-    path('token/verify', TokenVerifyView.as_view(), name='token_verify'), # token検証
-    path("user/",include("user.urls")),
+    path("login/", TokenObtainPairView.as_view(), name="login"),
+    path(
+        "token/refresh", TokenRefreshView.as_view(), name="token_refresh"
+    ),  # token更新
+    path("token/verify", TokenVerifyView.as_view(), name="token_verify"),  # token検証
+    path("user/", include("user.urls")),
 ]
--- src/user/models.py
+++ src/user/models.py
@@ -20,18 +20,20 @@ from utils.models import BaseModel
 class UserManager(BaseUserManager):
     """ユーザーのマネージャーモデル"""
 
-    def create_user(self, email:str, password=None, **extra_fields):
+    use_in_migrations = True
+
+    def create_user(self, email:str, name:str, password:str, **extra_fields):
         """新しいユーザー作成し､保存する"""
         if not email:
             raise ValueError("メールアドレスが必要です｡")
-        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user = self.model(name,email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
         return user
 
-    def create_superuser(self, email, password):
-        user = self.create_user(email, password)
+    def create_superuser(self,  email:str, name:str, password:str, **extra_fields):
+        user = self.create_user(name,self.normalize_email(email), password)
         user.is_staff = True
         user.is_superuser = True
         user.save(using=self._db)
@@ -42,14 +44,16 @@ class UserManager(BaseUserManager):
 class User(AbstractBaseUser, PermissionsMixin, BaseModel):
     """ユーザーモデル"""
 
-    email = models.EmailField(max_length=255, unique=True)
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255,blank=False,verbose_name="ユーザーネーム")
+    avatar=models.ImageField(upload_to="",null=True,blank=True,verbose_name="アバター")
+    email = models.EmailField(max_length=255, unique=True,verbose_name="メールアドレス")
     is_active = models.BooleanField(default=True, verbose_name="ユーザーの有効状態")
     is_staff = models.BooleanField(default=False, verbose_name="スタッフ権限")
 
     objects = UserManager()
 
-    USERNAME_FIELD = "email"
+    USERNAME_FIELD = "name"
+    REQUIRED_FIELDS=["email", "name"]
 
     class Meta:
         db_table = "users"
